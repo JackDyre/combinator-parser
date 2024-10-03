23fef0ac1993297bd2e6c54b49f9955ded8461f3
JackDyre <83248599+JackDyre@users.noreply.github.com>
2024-10-03 18:52:41 +0000

    some committ msg

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-#[derive(Debug)]
+use std::fmt;
+
+#[derive(Debug, Eq, PartialEq)]
 pub struct CombinatorExpression {
     pub exp: Vec<Combinator>,
 }
@@ -12,15 +14,20 @@ impl CombinatorExpression {
         for char in string.chars() {
             if char == '(' {
                 paren_depth += 1;
+                if paren_depth > 1 {
+                    sub_exp.push(char)
+                }
                 continue;
             }
             if char == ')' {
                 paren_depth -= 1;
                 if paren_depth == 0 {
                     let sub_string: String = sub_exp.clone().into_iter().collect();
-                    exp.push(Combinator::Expression(Self::parse(sub_string).unwrap()))
+                    exp.push(Combinator::Expression(Self::parse(sub_string).unwrap()));
+                    sub_exp = Vec::new();
+                } else {
+                    sub_exp.push(char)
                 }
-                sub_exp = Vec::new();
                 continue;
             }
             if paren_depth == 0 {
@@ -33,9 +40,20 @@ impl CombinatorExpression {
 
         Ok(Self { exp })
     }
+
+    pub fn contains(&self, var: &str) -> bool {
+        self.exp.iter().any(|a| match a {
+            Combinator::Single(e) => e == var,
+            Combinator::Expression(e) => e.contains(var),
+            Combinator::Abstraction {
+                exp: e,
+                variable: _,
+            } => e.contains(var),
+        })
+    }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 pub enum Combinator {
     Single(String),
     Expression(CombinatorExpression),
@@ -44,4 +62,84 @@ pub enum Combinator {
         variable: String,
     },
 }
+pub struct CombinatorDefinition {
+    pub def: CombinatorExpression,
+    pub args: Vec<String>,
+    pub name: String,
+}
 
+impl CombinatorDefinition {
+    pub fn new(args: Vec<String>, exp: String, name: String) -> anyhow::Result<Self> {
+        let def = CombinatorExpression::parse(exp)?;
+        Ok(Self { def, args, name })
+    }
+
+    /// $Fabc...wx:=f$ is equivalent to $Fabc...w:=[f]_x$ where
+    /// - $[x]_x=I$
+    /// - $[f]_x=K$, if $f$ does not conain $x$
+    /// - $[gx]_x=g$, if $g$ does not conatin $x$
+    /// - $[fg]_x=Bf[g]_x$, if $f$ does not contain $x$ but $g$ does
+    /// - $[fg]_x=C[f]_xg$, if $f$ does contains $x$ but $g$ does not
+    /// - $[fg]_x=S[f]_x[g]_x$, if both $f$ and $g$ contain $x$
+    pub fn abstraction_elimination(mut self) -> Self {
+        if self.args.len() == 0 {
+            return self;
+        }
+        let arg = self.args.pop().unwrap();
+
+        self.def = vec![Combinator::Abstraction {
+            exp: self.def,
+            variable: arg.clone(),
+        }]
+        .into();
+
+        while self.def.exp.iter().any(|a| match a {
+            Combinator::Abstraction {
+                exp: _,
+                variable: _,
+            } => true,
+            _ => false,
+        }) {
+            break;
+        }
+
+        self
+    }
+}
+
+impl fmt::Display for Combinator {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Combinator::Single(s) => write!(f, "{}", s),
+            Combinator::Expression(exp) => write!(f, "({})", exp),
+            Combinator::Abstraction { exp, variable } => write!(f, "[{}]_{}", exp, variable),
+        }
+    }
+}
+
+impl fmt::Display for CombinatorExpression {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for combinator in self.exp.iter() {
+            write!(f, "{}", combinator)?;
+        }
+        Ok(())
+    }
+}
+
+impl From<Vec<Combinator>> for CombinatorExpression {
+    fn from(value: Vec<Combinator>) -> Self {
+        Self { exp: value }
+    }
+}
+
+impl fmt::Display for CombinatorDefinition {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.name)?;
+        for arg in self.args.iter() {
+            write!(f, "{}", arg)?;
+        }
+        write!(f, "={}", self.def)?;
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+use combinator_parser::CombinatorDefinition;
 use combinator_parser::CombinatorExpression;
 
 fn main() {
-    let c = CombinatorExpression::parse("S(KS)K".to_string()).unwrap();
-    dbg!(c);
+    let c = CombinatorExpression::parse("x".to_string()).unwrap();
+    let c = CombinatorDefinition {
+        name: "Q".to_string(),
+        args: vec!["x".to_string()],
+        def: c,
+    };
+    println!("{c}");
+    let c = c.abstraction_elimination();
+    println!("{c}");
 }
