55b07f4b40d6a863b2d75b4c0ef026a817e9e9fa
JackDyre <83248599+JackDyre@users.noreply.github.com>
2024-10-09 19:41:32 +0000

    Combinator struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,73 @@
+use anyhow::Result;
+use nom::{
+    branch::alt,
+    character::complete::{alphanumeric1, char, satisfy},
+    combinator::{all_consuming, map, recognize},
+    multi::{many0, many1},
+    sequence::{delimited, terminated, tuple},
+    IResult,
+};
 
+#[derive(Debug, Clone)]
+pub enum Element {
+    Item(String),
+    SubExpression(Vec<Element>),
+}
+
+#[derive(Debug, Clone)]
+
+pub struct Combinator {
+    pub name: String,
+    pub args: Vec<String>,
+    pub expression: Expression,
+}
+
+impl Combinator {
+    pub fn parse(s: &str) -> Result<Self> {
+        let (rest, (name, args)) = declaration(s).unwrap();
+        let (_, exp) = all_consuming(expression)(rest).unwrap();
+
+        Ok(Self {
+            name: name.to_string(),
+            args: args.into_iter().map(|a| a.to_string()).collect(),
+            expression: Expression(exp),
+        })
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Expression(Vec<Element>);
+
+pub fn expression(s: &str) -> IResult<&str, Vec<Element>> {
+    many1(element)(s)
+}
+
+fn element(s: &str) -> IResult<&str, Element> {
+    alt((exp_item, subexpression))(s)
+}
+
+fn exp_item(s: &str) -> IResult<&str, Element> {
+    map(item, |i| Element::Item(i.to_string()))(s)
+}
+
+fn subexpression(s: &str) -> IResult<&str, Element> {
+    map(delimited(char('('), expression, char(')')), |e| {
+        Element::SubExpression(e)
+    })(s)
+}
+
+pub fn declaration(s: &str) -> IResult<&str, (&str, Vec<&str>)> {
+    terminated(tuple((item, many0(item))), char('='))(s)
+}
+
+fn item(s: &str) -> IResult<&str, &str> {
+    alt((alphanumeric_char, curly_brace_delimited))(s)
+}
+
+fn alphanumeric_char(s: &str) -> IResult<&str, &str> {
+    recognize(satisfy(|c| c.is_alphanumeric()))(s)
+}
+
+fn curly_brace_delimited(s: &str) -> IResult<&str, &str> {
+    delimited(char('{'), alphanumeric1, char('}'))(s)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,12 @@
 use anyhow::Result;
-use nom::{
-    branch::alt,
-    character::complete::{alphanumeric1, char, satisfy},
-    combinator::{all_consuming, map, recognize},
-    multi::{many0, many1},
-    sequence::{delimited, terminated, tuple},
-    IResult,
-};
+use combinator_parser::Combinator;
 
 fn main() -> Result<()> {
-    let input_str = "Qfgx=f(gx)(gx)!";
+    let input_str = "Qfgx=f(gx)(gx)";
 
-    let (rest, (_name, _args)) = declaration(input_str)?;
+    let c = Combinator::parse(input_str)?;
 
-    // println!("{input_str:?}");
-    // println!("{name:?}");
-    // println!("{args:?}");
-    // println!("{rest:?}");
-
-    let (rest, exp) = all_consuming(expression)(rest)?;
-
-    println!("{exp:?}");
-    println!("{rest:?}");
+    println!("{c:?}");
 
     Ok(())
-}
-
-fn expression(s: &str) -> IResult<&str, Vec<Element>> {
-    many1(element)(s)
-}
-
-fn element(s: &str) -> IResult<&str, Element> {
-    alt((exp_item, subexpression))(s)
-}
-
-fn exp_item(s: &str) -> IResult<&str, Element> {
-    map(item, |i| Element::Item(i.to_string()))(s)
-}
-
-fn subexpression(s: &str) -> IResult<&str, Element> {
-    map(delimited(char('('), expression, char(')')), |e| {
-        Element::SubExpression(e)
-    })(s)
-}
-
-fn declaration(s: &str) -> IResult<&str, (&str, Vec<&str>)> {
-    terminated(tuple((item, many0(item))), char('='))(s)
-}
-
-fn item(s: &str) -> IResult<&str, &str> {
-    alt((alphanumeric_char, curly_brace_delimited))(s)
-}
-
-fn alphanumeric_char(s: &str) -> IResult<&str, &str> {
-    recognize(satisfy(|c| c.is_alphanumeric()))(s)
-}
-
-fn curly_brace_delimited(s: &str) -> IResult<&str, &str> {
-    delimited(char('{'), alphanumeric1, char('}'))(s)
-}
-
-#[derive(Debug)]
-enum Element {
-    Item(String),
-    SubExpression(Vec<Element>),
 }
